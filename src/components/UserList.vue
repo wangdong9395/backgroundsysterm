263087f73261e57ae14b111a962025b21557289c
wangdong9395 <wangdong9395@163.com>
2020-04-26 22:07:02 +0800

    完成home页面布局和菜单导航功能

---
+++ src/components/UserList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>这是用户列表页111</div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
